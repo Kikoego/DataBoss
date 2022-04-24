0514dd6367742eff413a13ac496d2db483d2a0df
Kikoego <97868051+Kikoego@users.noreply.github.com>
2022-04-25 00:11:39 +0700

    Add files via upload

---
+++ project/controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+	namespace Project\Controllers;
+	use \Core\Controller;
+	
+	class TestController extends Controller
+	{
+		public function act1()
+		{
+			echo 'Rabotaet 1';
+		}
+		
+		public function act2()
+		{
+			echo 'Rabotaet 2';
+		}
+
+        public function act3()
+		{
+			echo 'Rabotaet 3';
+		}
+    }
+?>
